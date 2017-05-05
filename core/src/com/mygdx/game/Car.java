6609e701f61b9ea6755e64afaa83f725e9514797
dino <dino@330z.net>
2017-05-06 02:40:50 +0930

    implement click to change direction

--- core/src/com/mygdx/game/Car.java
+++ core/src/com/mygdx/game/Car.java
@@ -3,10 +3,14 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.BaseDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 /**
  * Created by dino on 29/4/17.
@@ -21,6 +25,9 @@ public class Car {
     private Texture leftTexture;
     private Texture rightTexture;
 
+//    private Drawable leftDrawable;
+//    private Drawable rightDrawable;
+
     private Sprite playerSprite;
     Vector2 playerDelta;
     Rectangle playerDeltaRectangle;
@@ -34,7 +41,7 @@ public class Car {
         this.leftTexture = new Texture("Car/L.png");
         this.rightTexture = new Texture("Car/R.png");
         this.testImage = new Image(rightTexture);
-        testImage.setSize(MainGame.GRID_SIZE, MainGame.GRID_SIZE);
+        this.testImage.setSize(MainGame.GRID_SIZE, MainGame.GRID_SIZE);
         this.stage.addActor(testImage);
 
         this.initialCameraY = stage.getViewport().getCamera().position.y;
@@ -44,8 +51,16 @@ public class Car {
 
     }
 
-    public void updateAndRender(float deltaTime){
+    public void changeDirection(){
+        facingLeft = !facingLeft;
+        if(facingLeft)
+            testImage.setDrawable(new TextureRegionDrawable(new TextureRegion(leftTexture)));
+        else
+            testImage.setDrawable(new TextureRegionDrawable(new TextureRegion(rightTexture)));
+//        this.testImage.setSize(MainGame.GRID_SIZE, MainGame.GRID_SIZE);
+    }
 
+    public void updateAndRender(float deltaTime){
         if (stage.getViewport().getCamera().position.y - initialCameraY >= stage.getViewport().getCamera().viewportHeight)
             //bring the car back to the original point when map refresh
             testImage.setY(testImage.getY() - stage.getHeight());
@@ -53,6 +68,12 @@ public class Car {
             //keep moving the car with the camera
             testImage.setY(testImage.getY() + (100.0f * deltaTime));
 
+        if (facingLeft)
+            //move toward left
+            testImage.setX(testImage.getX() - (200.0f * deltaTime));
+        else
+            //love toward right
+            testImage.setX(testImage.getX() + (200.0f * deltaTime));
     }
 
     public void resize(int width, int height){
--- core/src/com/mygdx/game/MainGame.java
+++ core/src/com/mygdx/game/MainGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
@@ -15,7 +16,7 @@ import com.badlogic.gdx.utils.viewport.ScalingViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class MainGame extends ApplicationAdapter {
+public class MainGame extends ApplicationAdapter implements InputProcessor {
     public static final int DEFAULT_SPEED = 80;
     public static final int ACCELERATION = 50;
     public static final int GOAL_REACH_ACCELERATION = 200;
@@ -53,6 +54,8 @@ public class MainGame extends ApplicationAdapter {
         bridge = new Bridge(rowNum, colNum, bridgeWidth, stage);
         car = new Car(stage);
 
+        Gdx.input.setInputProcessor(this);
+
 	}
 
 	@Override
@@ -88,4 +91,45 @@ public class MainGame extends ApplicationAdapter {
         this.bridge.resize(width, height);
         this.car.resize(width, height);
 	}
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        this.car.changeDirection();
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
